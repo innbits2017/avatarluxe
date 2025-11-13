4c00d730a342d8e51cbd31f7390c4330ecc7b81e
innbits2017 <innbits@gmail.com>
2025-11-13 09:18:12 +0530

    landing

--- src/app/page.js
+++ src/app/page.js
@@ -180,7 +180,7 @@ export default function Home() {
           className="text-4xl sm:text-5xl md:text-7xl lg:text-9xl font-semibold text-center text-transparent bg-clip-text bg-gradient-to-b from-white to-gray-400 z-30 relative px-4 -mt-[50px]"
           style={{ letterSpacing: "0.2em" }}
         >
-          AVATARLUXE
+          AVATARLUX
         </motion.h1>
       </section>
 
